824a932243116e3ae1cd3f743a2e7148c9421342
Juan Pablo Abadia Medina <88802339+juanpabloabadiamedina@users.noreply.github.com>
2021-08-12 21:01:19 -0500

    Add files via upload

---
+++ Afghanistan.php
@@ -0,0 +1,555 @@
+<?php
+/* provincias */
+
+$AF = array(
+    "Badahšan" => $Badahšan,
+    "Bādgīs" => $Bādgīs,
+    "Baġlān" => $Baġlān, 
+    "Balh" => $Balh,
+    "Bāmiyān" => $Bāmiyān,
+    "Daikondi" => $Daikondi,  
+    "Farāh" => $Farāh,  
+    "Fāryāb" => $Fāryāb,   
+    "Ġaznī" => $Ġaznī,
+    "Ġawr" => $Ġawr,    
+    "Jost" => $Jost,
+    "Helmand" => $Helmand,
+    "Herāt" => $Herāt,
+    "Jawzjān" => $Jawzjān,
+    "Kabul" => $Kabul,
+    "Kandahar" => $Kandahar,
+    "Kāpīsā" => $Kāpīsā,
+    "Kunar" => $Kunar,
+    "Laġmān" => $Laġmān,
+    "Lawgar" => $Lawgar,
+    "Nangarhar" => $Nangarhar,
+    "Nimruz" => $Nimruz,
+    "Nūristān" => $Nūristān,
+    "Paktiyā" => $Paktiyā,
+    "Paktīkā" => $Paktīkā,
+    "Panjshīr" => $Panjshīr,
+    "Parvān" => $Parvān,
+    "Qundūz" => $Qundūz,
+    "Samangān" => $Samangān,
+    "Sar-e Pul" => $SarePul,
+    "Tahār" => $Tahār,
+    "Urūzgān" => $Urūzgān,
+    "Vardak" => $Vardak,
+    "Zābul" => $Zābul
+    
+);
+
+/* Zābul */
+$Zābul = array(
+    "Arghandab" => "Arghandab",
+    "Atghar" => "Atghar",
+    "Daychopan" => "Daychopan",
+    "Mizan" => "Mizan",
+    "Qalat-i-Ghilzai" => "Qalat-i-Ghilzai",
+    "Shahjoy" => "Shahjoy",
+    "Shamulzayi" => "Shamulzayi",
+    "Shinkay" => "Shinkay",
+    "Tarnak Wa Jaldak" => "Tarnak Wa Jaldak"
+);
+
+/* Vardak */
+$Vardak = array(
+    "Maydan Shahr" => "Maydan Shahr",
+    "Jalrez" => "Jalrez",
+    "Hisa-I- Awali Bihsud" => "Hisa-I- Awali Bihsud",
+    "Markazi Bihsud" => "Markazi Bihsud",
+    "Day Mirdad" => "Day Mirdad",
+    "Chaki Wardak" => "Chaki Wardak",
+    "Saydabad" => "Saydabad",
+    "Nirkh" => "Nirkh",
+    "Jaghatu" => "Jaghatu"
+);
+
+/* Urūzgān */
+$Urūzgān = array(
+    "Chora" => "Chora",
+    "Dihrawud" => "Dihrawud",
+    "Gizab" => "Gizab",
+    "Khas Uruzgan" => "Khas Uruzgan",
+    "Nesh" => "Nesh",
+    "Shahidi Hassas" => "Shahidi Hassas",
+    "Tirin Kot" => "Tirin Kot"
+);
+
+/* Tahār */
+$Tahār = array(
+    "Darqad" => "Darqad",
+    "Chah Ab" => "Chah Ab",
+    "Yangi Qala" => "Yangi Qala",
+    "Rustaq" => "Rustaq",
+    "Khwaja Ghar" => "Khwaja Ghar",
+    "Kalafgan" => "Kalafgan",
+    "Taluqan" => "Taluqan",
+    "Bangi" => "Bangi",
+    "Farkhar" => "Farkhar",
+    "Distrito de Chal" => "Distrito de Chal",
+    "Ishkamish" => "Ishkamish",
+    "Warsaj" => "Warsaj",
+    "Khwaja Bahauddin" => "Khwaja Bahauddin",
+    "Dashti Qala" => "Dashti Qala",
+    "Baharak" => "Baharak",
+    "Hazar Sumuch" => "Hazar Sumuch",
+    "Namak ab1​" => "Namak ab1​"
+);
+
+/* SarePul */
+$SarePul = array(
+    "Sayyad" => "Sayyad",
+    "Sari Pul" => "Sari Pul",
+    "Sazma Qala" => "Sazma Qala",
+    "Sangcharak" => "Sangcharak",
+    "Dara-i- Suf" => "Dara-i- Suf",
+    "Balkhab" => "Balkhab",
+    "Virgaard" => "Virgaard"
+);
+
+/* Samangān */
+$Samangān = array(
+    "Hazrati Sultan" => "Hazrati Sultan",
+    "Aybak" => "Aybak",
+    "Khuram wa Sarbagh" => "Khuram wa Sarbagh",
+    "Ruyi du Ab" => "Ruyi du Ab",
+    "Dara-i- Suf" => "Dara-i- Suf"
+);
+
+/* Qundūz */
+$Qundūz = array(
+    "Ali abad" => "Ali abad",
+    "Aqtash" => "Aqtash",
+    "Dashte Archi" => "Dashte Archi",
+    "Chahar dara" => "Chahar dara",
+    "Imam Sahib" => "Imam Sahib",
+    "Khan Abad" => "Khan Abad",
+    "Qundūz" => "Qundūz",
+    "Qalay-I-Zal" => "Qalay-I-Zal"
+);
+
+/* Parvān */
+$Parvān = array(
+    "Shekj Ali" => "Shekj Ali",
+    "Surkhi Parsa" => "Surkhi Parsa",
+    "Ghorband" => "Ghorband",
+    "Shinwari" => "Shinwari",
+    "Kohi Safi" => "Kohi Safi",
+    "Bagram" => "Bagram",
+    "Chaharikar" => "Chaharikar",
+    "Jabalussaraj" => "Jabalussaraj"
+);
+
+/* Panjshīr */
+$Panjshīr = array(
+    "Salang (distrito)" => "Salang (distrito)",
+    "Panjsher" => "Panjsher",
+    "Hisa-i-Duwumi panjsher" => "Hisa-i-Duwumi panjsher",
+    "Hisa-i-Awali panjsher" => "Hisa-i-Awali panjsher",
+    "Crahaetun" => "Crahaetun"
+);
+
+
+/* Paktīkā */
+$Paktīkā = array(
+    "Barmal" => "Barmal",
+    "Dila" => "Dila",
+    "Gayan" => "Gayan",
+    "Gomal" => "Gomal",
+    "Mata Khan" => "Mata Khan",
+    "Nika" => "Nika",
+    "Omna" => "Omna",
+    "Sar Hawza" => "Sar Hawza",
+    "Sarobi" => "Sarobi",
+    "Sharan" => "Sharan",
+    "Urgun" => "Urgun",
+    "Waza Khwa" => "Waza Khwa",
+    "Zarghun Shahr" => "Zarghun Shahr",
+    "Ziruk" => "Ziruk",
+    "Ar-Bayarh" => "Ar-Bayarh"
+);
+
+/* Paktiyā */
+$Paktiyā = array(
+    "Azra (Sur Kac)" => "Azra (Sur Kac)",
+    "Tsamkani (Chamkani)" => "Tsamkani (Chamkani)",
+    "Dand Wa Patan (Ghondai)" => "Dand Wa Patan (Ghondai)",
+    "Gardez (Gardez)" => "Gardez (Gardez)",
+    "Zadran (Waza, Afghanistan)" => "Zadran (Waza, Afghanistan)",
+    "ZaZi (Ali Khel)" => "ZaZi (Ali Khel)",
+    "Jani Khel (Jani Khel)" => "Jani Khel (Jani Khel)",
+    "Lazha Mangal (Lazha)" => "Lazha Mangal (Lazha)",
+    "Sayed Karam (Seyyed Karam)" => "Sayed Karam (Seyyed Karam)",
+    "Shamal (Shamal)" => "Shamal (Shamal)",
+    "Shwak (Shwak)" => "Shwak (Shwak)",
+    "Zurmat (Zarmal)" => "Zurmat (Zarmal)",
+    "Ahmadzai (Ahmadaba)" => "Ahmadzai (Ahmadaba)"
+);
+
+/* Nūristān */
+$Nūristān = array(
+    "Bargi Matal" => "Bargi Matal",
+    "Kamdesh" => "Kamdesh",
+    "Mandol" => "Mandol",
+    "Nuristan" => "Nuristan",
+    "Wama" => "Wama",
+    "Waygal" => "Waygal"
+);
+
+/* Nimruz */
+$Nimruz = array(
+    "Chahar Burjak" => "Chahar Burjak",
+    "Chakhansur" => "Chakhansur",
+    "Kang" => "Kang",
+    "Khash Rod" => "Khash Rod",
+    "Zaranj" => "Zaranj"
+);
+
+/* Nangarhar */
+$Nangarhar = array(
+    "Jalalabad" => "Jalalabad",
+    "Behsud" => "Behsud",
+    "Surkh Rud" => "Surkh Rud",
+    "Chaparhar" => "Chaparhar",
+    "Kama" => "Kama",
+    "Kuzkunar" => "Kuzkunar",
+    "Rodat" => "Rodat",
+    "Khugyani" => "Khugyani",
+    "Bati Kot" => "Bati Kot",
+    "Deh Bala" => "Deh Bala",
+    "Pachir Waagam" => "Pachir Waagam",
+    "Darah-e-Noor" => "Darah-e-Noor",
+    "Kot" => "Kot",
+    "Goshta" => "Goshta",
+    "Achin" => "Achin",
+    "Shinwar" => "Shinwar",
+    "Muhmand Dara" => "Muhmand Dara",
+    "Lalpoor" => "Lalpoor",
+    "Sher Zad" => "Sher Zad",
+    "Nazyan" => "Nazyan",
+    "Hesarak" => "Hesarak",
+    "Dur Baba" => "Dur Baba"
+);
+
+/* Lawgar */
+$Lawgar = array(
+    "Baraki Barak" => "Baraki Barak",
+    "Charkh" => "Charkh",
+    "Khushi" => "Khushi",
+    "Mohamad Agha" => "Mohamad Agha",
+    "Kharwar" => "Kharwar",
+    "Azra" => "Azra",
+    "Pul-i-Alam" => "Pul-i-Alam"
+);
+
+/* Laġmān */
+$Laġmān = array(
+    "Alingar" => "Alingar",
+    "Alishing" => "Alishing",
+    "Dawlat Shah" => "Dawlat Shah",
+    "Mihtarlam" => "Mihtarlam",
+    "Qarghayi" => "Qarghayi"
+);
+
+
+/* Kunar */
+$Kunar = array(
+    "Asadabad" => "Asadabad",
+    "Marawara" => "Marawara",
+    "Bar Kunar" => "Bar Kunar",
+    "Dangam" => "Dangam",
+    "Nari" => "Nari",
+    "Ghaziabad" => "Ghaziabad",
+    "Shaygal Wa Shiltan" => "Shaygal Wa Shiltan",
+    "Wata Pur" => "Wata Pur",
+    "Chapa Dara" => "Chapa Dara",
+    "Dara-I-Pech" => "Dara-I-Pech",
+    "Narang" => "Narang",
+    "Chawkay" => "Chawkay",
+    "Nurgal" => "Nurgal",
+    "Khas Kunar" => "Khas Kunar",
+    "Sirkanay" => "Sirkanay"
+);
+
+/* Kāpīsā */
+$Kāpīsā = array(
+    "Alasay" => "Alasay",
+    "Koh Band" => "Koh Band",
+    "Kohistan" => "Kohistan",
+    "Mahmud Raqi" => "Mahmud Raqi",
+    "Nijrab" => "Nijrab",
+    "Tagab" => "Tagab"
+);
+
+/* Kandahar */
+$Kandahar = array(
+    "Arghandab" => "Arghandab",
+    "Arghistan" => "Arghistan",
+    "Daman" => "Daman",
+    "Ghorak" => "Ghorak",
+    "Khakrez" => "Khakrez",
+    "Maruf" => "Maruf",
+    "Maywand" => "Maywand",
+    "Panjwai" => "Panjwai",
+    "Reg" => "Reg",
+    "MShah Wali Kot" => "Shah Wali Kot",
+    "Shorabak" => "Shorabak",
+    "Spin Boldak" => "Spin Boldak",
+    "Dand" => "Dand",
+    "Miyannasheen" => "Miyannasheen",
+    "Zhari" => "Zhari",
+    "Naish" => "Naish"
+);
+
+/* Kabul */
+$Kabul = array(
+    "Bagrami" => "Bagrami",
+    "Chahar Asyab" => "Chahar Asyab",
+    "Dih Sabz" => "Dih Sabz",
+    "Guldara" => "Guldara",
+    "Istalif" => "Istalif",
+    "Kabul" => "Kabul",
+    "Kalakan" => "Kalakan",
+    "Khaki Jabbar" => "Khaki Jabbar",
+    "Mir Bacha Kot" => "Mir Bacha Kot",
+    "Musayi" => "Musayi",
+    "Paghman" => "Paghman",
+    "Qarabagh" => "Qarabagh",
+    "Shakar Dara" => "Shakar Dara",
+    "Surobi" => "Surobi"
+);
+
+/* Herāt */
+$Herāt = array(
+    "Āqčah" => "Āqčah",
+    "Darzab" => "Darzab",
+    "Fayzabad" => "Fayzabad",
+    "Khamyab" => "Khamyab",
+    "Khwaja Du Koh" => "Khwaja Du Koh",
+    "Mardyan" => "Mardyan",
+    "Mingajik" => "Mingajik",
+    "Qarqīn" => "Qarqīn",
+    "Šibarġan" => "Šibarġan"
+);
+
+/* Herāt */
+$Herāt = array(
+    "Adraskan" => "Adraskan",
+    "Chishti Sharif" => "Chishti Sharif",
+    "Farsi" => "Farsi",
+    "Ghoryan" => "Ghoryan",
+    "Gulran" => "Gulran",
+    "Guzara" => "Guzara",
+    "Hirat" => "Hirat",
+    "Injil" => "Injil",
+    "Karukh" => "Karukh",
+    "Kohsan" => "Kohsan",
+    "Kushk" => "Kushk",
+    "Kushki Kuhma" => "Kushki Kuhma",
+    "Obe" => "Obe",
+    "Pashtun Zarghun" => "Pashtun Zarghun",
+    "Shindand" => "Shindand",
+    "Zinda Jan" => "Zinda Jan"
+);    
+
+/* Helmand */
+$Helmand = array(
+    "Baghran" => "Baghran",
+    "Dishu" => "Dishu",
+    "Garmser" => "Garmser",
+    "Gerešk" => "Gerešk",
+    "Kajaki" => "Kajaki",
+    "Lashkar Gah" => "Lashkar Gah",
+    "Musa Qala" => "Musa Qala",
+    "Nad Ali" => "Nad Ali",
+    "Nahri Sarraj" => "Nahri Sarraj",
+    "Naw Zad" => "Naw Zad",
+    "Nawa-i-Barakzayi" => "Nawa-i-Barakzayi",
+    "Reg" => "Reg",
+    "Sangīn" => "Sangīn",    
+    "Washer" => "Washer"
+);
+
+/* Jost */
+$Jost = array(
+    "Bak" => "Bak",
+    "Gurbuz" => "Gurbuz",
+    "Jaji Maydan" => "Jaji Maydan",
+    "Khost" => "Khost",
+    "Mando Zayi" => "Mando Zayi",
+    "Sabari" => "Sabari",
+    "Musa Khel" => "Musa Khel",
+    "Nadir Shah Kot" => "Nadir Shah Kot",
+    "Qalandar" => "Qalandar",
+    "Spera" => "Spera",
+    "Tani" => "Tani",
+    "Tere Zayi" => "Tere Zayi"
+);
+
+/* Ġawr */
+$Ġawr = array(
+    "Chaghcharan" => "Chaghcharan",
+    "Lal Wa Sarjangal" => "Lal Wa Sarjangal",
+    "Pasaband" => "Pasaband",
+    "Shahrak" => "Shahrak",
+    "Taywara" => "Taywara",
+    "Tulak" => "Tulak"
+);
+
+
+/* Ġaznī */
+$Ġaznī = array(
+    "Ab Band" => "Ab Band",
+    "Ajristan" => "Ajristan",
+    "Bahrami Shahid" => "Bahrami Shahid",
+    "Dih Yak" => "Dih Yak",
+    "Gelan" => "Gelan",
+    "Ghazni" => "Ghazni",
+    "Giro" => "Giro",
+    "Jaghatu" => "Jaghatu",
+    "Jaghuri" => "Jaghuri",
+    "Malistan" => "Malistan",
+    "Miri" => "Miri",
+    "Muqur" => "Muqur",
+    "Nawa" => "Nawa",
+    "Nawur" => "Nawur",
+    "Qarabagh" => "Qarabagh",
+    "Zana Khan" => "Zana Khan"
+);
+
+/* Fāryāb */
+$Fāryāb = array(
+    "Almar" => "Almar",
+    "Andkhoy" => "Andkhoy",
+    "Bilchiragh" => "Bilchiragh",
+    "Dawlatabad" => "Dawlatabad",
+    "Khani Chahar Bagh" => "Khani Chahar Bagh",
+    "Khwaja Sabz Posh" => "Khwaja Sabz Posh",
+    "Kohistan" => "Kohistan",
+    "Maymana" => "Maymana",
+    "Pashtun Kot" => "Pashtun Kot",
+    "Qaramqol" => "Qaramqol",
+    "Qaysar" => "Qaysar",
+    "Shirin Tagab" => "Shirin Tagab"
+);
+
+
+/* Farāh */
+$Farāh = array(
+    "Anar Dara" => "Anar Dara",
+    "Bakwa" => "Bakwa",
+    "Bala Buluk" => "Bala Buluk",
+    "Farah" => "Farah",
+    "Gulistán" => "Gulistán",
+    "Khaki Safed" => "Khaki Safed",
+    "Lash Wa Juwayn" => "Lash Wa Juwayn",
+    "Pur Chaman" => "Pur Chaman",
+    "Pusht Rod" => "Pusht Rod",
+    "Shib Koh" => "Shib Koh"
+);
+
+/* Daikondi */
+$Daikondi = array(
+    "Gizab" => "Gizab",
+    "Ishtarlay" => "Ishtarlay",
+    "Kajran" => "Kajran",
+    "Khadir" => "Khadir",
+    "Kiti" => "Kiti",
+    "Miramor" => "Miramor",
+    "Nili" => "Nili",
+    "Sangi Takht" => "Sangi Takht",
+    "Shahristan" => "Shahristan"
+);
+    
+/* Bāmiyān */
+$Bāmiyān = array(
+    "Bamiyán" => "Bamiyán",
+    "Kahmard" => "Kahmard",
+    "Panjab" => "Panjab",
+    "Sayghan" => "Sayghan",
+    "Shibar" => "Shibar",
+    "Waras" => "Waras",
+    "Yakawlang" => "Yakawlang"
+);
+
+/* Balh */
+$Balh = array(
+    "Balkh" => "Balkh",
+    "Chahar Bolak" => "Chahar Bolak",
+    "Chahar Kint" => "Chahar Kint",
+    "Chimtal" => "Chimtal",
+    "Dawlatabad" => "Dawlatabad",
+    "Dihdadi" => "Dihdadi",
+    "Kaldar" => "Kaldar",
+    "Khulmi" => "Khulmi",
+    "Kishindih" => "Kishindih",
+    "Marmul" => "Marmul",
+    "Mazar-e Sharif" => "Mazar-e Sharif",
+    "Nahri Shahi" => "Nahri Shahi",
+    "Sholgara" => "Sholgara",
+    "Shortepa" => "Shortepa",
+    "Zari" => "Zari"
+);    
+
+/* Baġlān */
+$Baġlān = array(
+    "Andarāb" => "Andarāb",
+    "Baghlani Jadid" => "Baghlani Jadid", 
+    "Burka" => "Burka", 
+    "Dahana i Ghuri" => "Dahana i Ghuri", 
+    "Dih Salah" => "Dih Salah", 
+    "Dushi" => "Dushi", 
+    "Farang Wa Gharu" => "Farang Wa Gharu", 
+    "Guzargahi Nur" => "Guzargahi Nur", 
+    "Khinjan" => "Khinjan", 
+    "Khost Wa Fereng" => "Khost Wa Fereng", 
+    "Khwaja Hijran" => "Khwaja Hijran", 
+    "BNahrīn" => "Nahrīn", 
+    "Puli Hisar" => "Puli Hisar", 
+    "Puli Khumri" => "Puli Khumri", 
+    "Tala wa Barfak" => "Tala wa Barfak"    
+);
+
+/* Badahšan */
+$Badahšan = array(
+    "Arghanj Khwa" => "Arghanj Khwa",
+    "Argo" => "Argo",
+    "Baharak" => "Darayim",
+    "Darwaz" => "Darwaz",
+    "Darwazi Bala" => "Darwazi Bala",
+    "Fayzabad" => "Fayzabad",
+    "Ishkashim" => "Ishkashim",
+    "Jurm" => "Jurm",
+    "Khash" => "Khash",
+    "Khwahan" => "Khwahan",
+    "Kishim" => "Kishim",
+    "Kohistan" => "Kohistan",
+    "Kuf Ab" => "Kuf Ab",
+    "Kuran wa Munjan" => "Kuran wa Munjan",
+    "Ragh" => "Ragh",
+    "Shahri Buzurg" => "Shahri Buzurg",
+    "Shighnan" => "Shighnan",
+    "Shiki" => "Shiki",
+    "Shuhada" => "Shuhada",
+    "Tagab" => "Tagab",
+    "Tishkan" => "Tishkan",
+    "Wakhan" => "Wakhan",
+    "Warduj" => "Warduj",
+    "Yaftali Sufla" => "Yaftali Sufla",
+    "Yamgan" => "Yamgan",
+    "Yawan" => "Yawan",
+    "Zebak" => "Zebak"
+);
+
+/* Bādgīs */
+$Bādgīs = array(
+    "Ab Kamari" => "Ab Kamari",
+    "Ghormach" => "Ghormach", 
+    "Jawand" => "Jawand",   
+    "Muqur" => "Muqur",   
+    "Murghab" => "Murghab",   
+    "Qadis" => "Qadis",   
+    "Qal'eh-ye Now" => "Qal'eh-ye Now"   
+);
+?>
